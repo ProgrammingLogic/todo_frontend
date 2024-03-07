e74ff8dbddae8234eef53f6424dd58a3cdbcaa08
jstiverson <jlstiverson2002@protonmail.com>
2024-03-07 16:35:00 -0600

    Add filter buttons

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,27 @@ function App(props) {
             Add
           </button>
         </form>
+
+
+        <div className="filters btn-group stack-exception">
+          <button type="button" className="btn toggle-btn" aria-pressed="true">
+            <span className="visually-hidden">Show </span>
+            <span>All</span>
+            <span className="visually-hidden"> Tasks</span>
+          </button>
+
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Active</span>
+            <span className="visually-hidden"> Tasks</span>
+          </button>
+
+          <button type="button" className="btn toggle-btn" aria-pressed="false">
+            <span className="visually-hidden">Show </span>
+            <span>Completed</span>
+            <span className="visually-hidden"> Tasks</span>
+          </button>
+        </div>
       </div> 
     </>
   )
