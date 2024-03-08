2de01a04313a2fef4be0220213f3e94b0ce2778c
jstiverson <jlstiverson2002@protonmail.com>
2024-03-07 20:48:27 -0600

    Update addTask to append to the list, add dynamic task coutn

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => 
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks?.map((task) => (
     <Todo 
       id={task.id}
       name={task.name} 
       completed={task.completed}
       key={task.id}
     />
-  );
+  ));
 
 
   function addTask(name) {
-    alert(name);
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask])
   }
 
 
 
   let filter = "All";
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <>
@@ -38,7 +45,7 @@ function App(props) {
         </div>
 
 
-        <h2 id="list-heading">3 tasks remaining</h2>
+        <h2 id="list-heading">{headingText}</h2>
         <ul
           role="list"
           className="todo-list stack-large stack-exception"
