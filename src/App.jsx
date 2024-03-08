0b63c23e12115f51d14198c9f672a2700e4a0f35
jstiverson <jlstiverson2002@protonmail.com>
2024-03-07 22:18:46 -0600

    Add feature to filter tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,23 @@ import { nanoid } from "nanoid";
 import "./App.css";
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+
+  function updateFilter(filter) {
+    setFilter(filter);
+  }
 
 
   function toggleTaskCompleted(id) {
@@ -38,17 +53,29 @@ function App(props) {
   }
 
 
-  const taskList = tasks?.map((task) => (
-    <Todo 
-      id={task.id}
-      name={task.name} 
-      completed={task.completed}
-      key={task.id}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
+
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo 
+        id={task.id}
+        name={task.name} 
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
 
 
   function addTask(name) {
@@ -57,7 +84,6 @@ function App(props) {
   }
 
 
-  let filter = "All";
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
@@ -70,9 +96,7 @@ function App(props) {
 
 
         <div className="filters btn-group stack-exception">
-          <FilterButton name="All" filter={filter}/>
-          <FilterButton name="Active" filter={filter}/>
-          <FilterButton name="Completed" filter={filter}/>
+          {filterList}
         </div>
 
 
--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,9 +1,15 @@
-function FilterButton(props) {
-    let pressed = props.filter === props.name;
+import { useState } from "react";
 
+
+function FilterButton(props) {
     return (
         <>
-            <button type="button" className="btn toggle-btn" aria-pressed={pressed}>
+            <button 
+                type="button"
+                className="btn toggle-btn"
+                aria-pressed={props.isPressed}
+                onClick={() => props.setFilter(props.name)}
+            >
                 <span className="visually-hidden">Show </span>
                 <span>{props.name}</span>
                 <span className="visually-hidden"> Tasks</span>
