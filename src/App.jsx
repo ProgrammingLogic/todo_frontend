a3c9613cc222499b858371059f0b5b83096cd2fc
jstiverson <jlstiverson2002@protonmail.com>
2024-03-07 21:53:41 -0600

    Add feature to edit a task

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,17 @@ function App(props) {
   }
 
 
+  function editTask(id, newName) { 
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
   const taskList = tasks?.map((task) => (
     <Todo 
       id={task.id}
@@ -35,6 +46,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
