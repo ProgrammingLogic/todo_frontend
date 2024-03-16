aed15c417a5437428729ef84e23cbce027d7a327
Jonathyn Stiverson <43549238+ProgrammingLogic@users.noreply.github.com>
2024-03-15 22:38:44 -0500

    Add functionality to mark task as completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import List from "@mui/material/List";
 
+import { UseState, useState } from "react";
+
 import Task from "./components/Task";
 
+const SAMPLE_TASKS = [
+  {"key": 1, name: "I am task #1", completed: false},
+  {"key": 2, name: "I am task #2", completed: false},
+  {"key": 3, name: "I am task #3", completed: false},
+  {"key": 4, name: "I am task #4", completed: false},
+  {"key": 5, name: "I am task #5", completed: false},
+  {"key": 6, name: "I am task #6", completed: true},
+  {"key": 7, name: "I am task #7", completed: false},
+  {"key": 8, name: "I am task #8", completed: false},
+  {"key": 9, name: "I am task #9", completed: false},
+  {"key": 10, name: "I am task #10", completed: false},
+];
 
 const darkTheme = createTheme({
   palette: {
@@ -14,21 +28,21 @@ const darkTheme = createTheme({
 
 
 export default function App() {
-  const taskList = [
-    {"key": 1, name: "I am task #1", completed: false},
-    {"key": 2, name: "I am task #2", completed: false},
-    {"key": 3, name: "I am task #3", completed: false},
-    {"key": 4, name: "I am task #4", completed: false},
-    {"key": 5, name: "I am task #5", completed: false},
-    {"key": 6, name: "I am task #6", completed: true},
-    {"key": 7, name: "I am task #7", completed: false},
-    {"key": 8, name: "I am task #8", completed: false},
-    {"key": 9, name: "I am task #9", completed: false},
-    {"key": 10, name: "I am task #10", completed: false},
-  ];
+  const [taskList, setTaskList] = useState(SAMPLE_TASKS);
+
+  function completeTask(key) {
+    const newTaskList = taskList.map((task) => {
+      if (task.key === key) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTaskList(newTaskList);
+  }
 
   const tasks = taskList.map((task) => 
-    <Task key={task.key} task={task} />
+    <Task key={task.key} task={task} completeTask={completeTask}/>
   );
 
   return (
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,23 @@
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Checkbox from "@mui/material/Checkbox";
+
 
 export default function Task(props) {
     const task = props.task;
+    const completeTask = props.completeTask;
 
     return (
         <ListItem>
-            <ListItemText primary={`Key: ${task.key}`}/>
-            <ListItemText primary={`Name: ${task.name}`}/>
-            <ListItemText primary={`Completed: ${task.completed}`}/>
+            <ListItemIcon>
+                <Checkbox 
+                    edge="start"
+                    checked={task.completed}
+                    onChange={() => props.completeTask(task.key)}
+                />
+            </ListItemIcon>
+            <ListItemText primary={task.name}/>
         </ListItem>
     );
 }
