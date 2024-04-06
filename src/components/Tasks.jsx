b8979b18056aee5fcff4f20d0337c9a8720c9509
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2024-04-06 13:57:46 -0500

    Fix usage of useEffect hook to fix infinite reloads of the page

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -14,63 +14,69 @@ import Task from "./Task";
 // - Write handleDelete() function to delete tasks
 // - Move task list handling into a file named TaskList.jsx
 export default function Tasks() {
-  const [data, setData] = useState([]);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetchTasks();
+    return(() => {
+      fetchTasks();
+    });
   }, []);
 
   const fetchTasks = () => {
-    const tasksData = [
+    const sampleTasks = [
       {id: 1, name: "I am task #1", completed: false},
       {id: 2, name: "I am task #2", completed: false},
       {id: 3, name: "I am task #3", completed: false},
       {id: 4, name: "I am task #4", completed: true},
     ];
 
-    setData(tasksData);
-    setTasks(data.map(task => {
-        <Task 
-            id={task.id}
-            name={task.name}
-            completed={task.completed}
-            handleUpdate={handleUpdate}
-            handleDelete={handleDelete}
-            handleComplet={handleComplete}
-        />
-    }));
+    setTasks(sampleTasks);
   };
 
   // item.id -- The ID of the task
   // item.name -- The name of the task
   // item.isCompleted -- Whether or not the item is completed
   const handleCreate = (item) => {
-    const newItem = {...item, id: data.length + 1};
-    setData([...data, newItem]);
+    const newItem = {...item, id: tasks.length + 1};
+    setTasks([...tasks, newItem]);
   };
 
   const handleUpdate = (id, newName) => {
-    const newData = data.map((item) => item.id === id ? {...item, name: newName} : item);
-    setData(newData);
+    const newTasks = tasks.map((item) => item.id === id ? {...item, name: newName} : item);
+    setTasks(newTasks);
   };
 
   const handleDelete = (id) => {
-    const newData = data.map(item => item !== id);
-    setData(newData);
+    // this deletes ALL tasks 
+    // fix this lol
+    const newTasks = tasks.map(item => item !== id);
+    setTasks(newTasks);
   }
 
   const handleComplete = (id) => {
-    const newData = data.map((item) => item.id === id ? {...item, completed: !item.completed} : item);
-    setData(newData);
+    const newTasks = tasks.map((item) => item.id === id ? {...item, completed: !item.completed} : item);
+    setTasks(newTasks);
   }
+
+  const listOfTasks = (
+      tasks.map((task) => (
+        <>
+          <Task 
+              id={task.id}
+              name={task.name}
+              completed={task.completed}
+              onUpdate={handleUpdate}
+              onDelete={handleDelete}
+              onComplete={handleComplete}
+          />
+        </>
+      ))
+  );
 
   return (
     <Stack alignItems="center" spacing={1}>
         <h1>Tasks</h1>
-        <ul>
-            <li>I am a task!</li>
-        </ul>
+        {listOfTasks}
     </Stack>
   )
 }
